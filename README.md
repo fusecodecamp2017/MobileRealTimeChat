d77cf83e5e419a94db31345634eeee52ff59f494
John Ryan <john.ryan@cardinalhealth.com>
2017-02-05 18:59:17 -0500

    Nearly there with setting up the home page.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 List of projected features for this application:
-1. Setup firebase as a data source
+1. Setup firebase as a data source.
+2. Setup home page to display and send messages.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { Message } from '../../models/message'
+import { MessageService } from '../../providers/message-service'
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [ MessageService ]
 })
 export class HomePage {
+  public USER_NAME_CONSTANT = 'John Ryan';
+  public currentMessage: string;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, private messageService: MessageService) {
   }
 
+  public send() {
+    this.buildAndSendMessage(this.currentMessage);
+    this.currentMessage = "";
+  }
+
+  private buildAndSendMessage(message: string) {
+      var newMessage = new Message();
+      newMessage.userName = this.USER_NAME_CONSTANT;
+      newMessage.messageContent = this.currentMessage;
+      this.messageService.addMessage(newMessage);
+  }
 }
