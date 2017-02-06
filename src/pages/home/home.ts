0a452ee73b22c5912234775a1080cfa36c66cfa2
John Ryan <john.ryan@cardinalhealth.com>
2017-02-05 19:17:00 -0500

    setup home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Message } from '../../models/message'
 import { MessageService } from '../../providers/message-service'
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [ MessageService ]
+  providers: [MessageService]
 })
 export class HomePage {
   public USER_NAME_CONSTANT = 'John Ryan';
@@ -15,15 +16,19 @@ export class HomePage {
   constructor(public navCtrl: NavController, private messageService: MessageService) {
   }
 
+  public formatDateTo_hhmm(dateProvidedAsString: string) {
+    return moment(new Date(dateProvidedAsString)).format('h:mm a');
+  }
+
   public send() {
     this.buildAndSendMessage(this.currentMessage);
     this.currentMessage = "";
   }
 
   private buildAndSendMessage(message: string) {
-      var newMessage = new Message();
-      newMessage.userName = this.USER_NAME_CONSTANT;
-      newMessage.messageContent = this.currentMessage;
-      this.messageService.addMessage(newMessage);
+    var newMessage = new Message();
+    newMessage.userName = this.USER_NAME_CONSTANT;
+    newMessage.messageContent = this.currentMessage;
+    this.messageService.addMessage(newMessage);
   }
 }
