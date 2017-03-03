1ab974134aaefdddabbb53175d51f6e5f62cbf6c
John Ryan <john.ryan@cardinalhealth.com>
2017-03-03 17:10:43 -0500

    rough initial commit

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { UserService } from '../../providers/user-service'
+import { NotificationService } from '../../providers/notification-service'
 import { User } from '../../models/user'
+import { Camera } from 'ionic-native';
+
+export const cameraOptionsForUserPicture = {
+  destinationType: Camera.DestinationType.DATA_URL,
+  targetWidth: 400,
+  targetHeight: 300
+};
+export const imageContentPrefix = 'data:image/jpeg;base64,';
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html'
+  templateUrl: 'login.html',
+  providers: [NotificationService]
 })
 export class LoginPage {
   public userBeingEdited: User;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserService, public notificationService: NotificationService) {
     this.setupUserBeingEdited()
   }
 
@@ -19,6 +29,14 @@ export class LoginPage {
     } else {
       this.userBeingEdited = new User();
     }
+  }
+
+  public capturePhoto() {
+    Camera.getPicture(cameraOptionsForUserPicture).then((imageData) => {
+      this.userBeingEdited.photo = imageContentPrefix + imageData.replace(/[\n\r]/g, '');
+    }, (error) => {
+      this.notificationService.showMessage('photo capture error: ' + JSON.stringify(error));
+    });
   }
 
   public save() {
--- src/providers/user-service.ts
+++ src/providers/user-service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { User } from '../models/user'
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class UserService {
+  public users: FirebaseListObservable<any>;
   public currentUser: User;
 
-  constructor(private storage: Storage) {
+  constructor(private storage: Storage, private angularFire: AngularFire) {
+    this.users = angularFire.database.list('/users');
   }
 
   public setupCurrentUser(){
     return new Promise((resolve, reject) => {
-      this.storage.get('currentUser').then((returnedUser) => {
-        this.currentUser = returnedUser;
-        console.log('UserService currentUser set');
-        resolve(this.currentUser);
+      this.storage.get('currentUserCached').then((userStoredLocally) => {
+        if (userStoredLocally) {
+          this.getUserFromFireBase(userStoredLocally).then((firebaseUserInfo) => {
+            this.currentUser = <User>firebaseUserInfo;
+            resolve();
+          });
+        } else {
+          this.currentUser = null;
+          resolve();
+        }
       }, () => {
         this.currentUser = null;
         reject(null);
@@ -23,9 +32,19 @@ export class UserService {
     });
   }
 
+  public getUserFromFireBase(user: User) {
+    return new Promise((resolve, reject) => {
+      let userSubscription = this.angularFire.database.object('/users/' + user.$key).subscribe((userData) => {
+          resolve(userData);
+          userSubscription.unsubscribe();
+      });
+    });
+  }
+
   public registerCurrentUser(updatedCurrentUserInfo: User) {
     this.currentUser = updatedCurrentUserInfo;
-    this.storage.set('currentUser', this.currentUser);
+    this.users.push(updatedCurrentUserInfo);
+    this.storage.set('currentUserCached', this.currentUser);
   }
 
   public clearCurrentUser() {
